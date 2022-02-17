24a791e65323cba8b882f657e20a4b0ad1371ca9
Severin Rosner <severin.rosner@htl.rennweg.at>
2022-02-17 09:45:51 +0100

    Created project and added JavaTetris_Pflichtenheft_v1.pdf

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module at.javatetris.project.javatetris {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens at.javatetris.project.javatetris to javafx.fxml;
+    exports at.javatetris.project.javatetris;
+}
