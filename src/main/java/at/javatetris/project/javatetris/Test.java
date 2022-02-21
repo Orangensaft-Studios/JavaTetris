daa4cd0606f17933cf05300b374e5b22547d1a1d
Severin Rosner <severin.rosner@htl.rennweg.at>
2022-02-21 08:50:40 +0100

    test

---
+++ src/main/java/at/javatetris/project/javatetris/Test.java
@@ -0,0 +1,11 @@
+package at.javatetris.project.javatetris;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
